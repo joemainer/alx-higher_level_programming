e485725e5e98a7875386ea97e817035e92ab5262
joemainer <mercywkiarie@gmail.com>
2023-04-28 18:17:26 -0700

    w32435

--- 0-rectangle.py
+++ 0-rectangle.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
-"""Rectangle Class.
-This module contains an empty class that defines a rectangle.
-Usage Example:
-    Rectangle = __import__('0-rectangle').Rectangle
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
+"""
+Defines an empty class Rectangle
 """
 
 
 class Rectangle:
-    """Defines the blueprint of a rectangle"""
-
+    """Empty representation of a rectangle"""
     pass
